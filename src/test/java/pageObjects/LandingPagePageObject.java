150b9e7146bfd1cb25b93d98909ef58b368d6f4a
pravin <kadampravin05@gmail.com>
2021-08-14 23:03:13 +0530

    Add Product catageroy validation test

--- src/test/java/pageObjects/LandingPagePageObject.java
+++ src/test/java/pageObjects/LandingPagePageObject.java
@@ -1,6 +1,11 @@
 package pageObjects;
 
+import java.util.Iterator;
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LandingPagePageObject {
@@ -8,9 +13,21 @@ public class LandingPagePageObject {
 	WebDriver driver;
 	WebDriverWait wait;
 
+	public LandingPagePageObject(WebDriver driver) 
+	{
+	this.driver = driver;
+	}
+	private By productCatageroyLocator = By.xpath("//div[@id='block_top_menu']/ul/li/a");
 
 
+	public void productlist() {
 
+	List <WebElement> productList = driver.findElements(productCatageroyLocator);
+	
+	for(WebElement e : productList)
+	   {System.out.println(e);}
+	
+	}
 
 
 
