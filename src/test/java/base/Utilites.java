8efa2133e4153b830b6eabd1a0e499d9a4b0af9d
pravin <kadampravin05@gmail.com>
2021-08-15 21:45:36 +0530

    Formatting Code

--- src/test/java/base/Utilites.java
+++ src/test/java/base/Utilites.java
@@ -10,35 +10,37 @@ public class Utilites {
 	WebDriver driver;
 	WebDriverWait wait;
 
-	public Utilites(WebDriver driver) {
+	public Utilites(WebDriver driver,Scenario scn) {
 
 		this.driver = driver;
-
+		this.scn = scn;
 	}
 
 	public void enterUrl(String url) {
 		driver.get(url);
-
+		//  scn.log("Driver initialized successfully.");
 	}
 
 	public void assertUrl(String url) {
 		Assert.assertEquals("Url assertion faild", url, driver.getCurrentUrl());
 		System.out.println("User successfully asserted url and test case passed");
-
+		//	scn.log("asserting url " + url+ " with courrent page url.");
 	}
 
 	public void getCurentPageTittle() {
 		driver.getTitle();
+		//	scn.log("user faching current page title :" + driver.getTitle());
 	}
 
 	public void assertPageTitle(String title) {
 		Assert.assertEquals("user unble to asserting page title ", title, driver.getTitle());
 		System.out.println("User Assert Page Title successfully Test case passed.");
+		//	scn.log("User assert Curent page tile succesfully.");
 	}
 
 	public void closeDriver() {
 		driver.quit();
-
+		//	scn.log("Browser colsed.");
 	}
 
 
--- src/test/java/pageObjects/LandingPagePageObject.java
+++ src/test/java/pageObjects/LandingPagePageObject.java
@@ -8,28 +8,46 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import io.cucumber.java.Scenario;
+
 public class LandingPagePageObject {
 
+	Scenario scn;
 	WebDriver driver;
 	WebDriverWait wait;
 
-	public LandingPagePageObject(WebDriver driver) 
+
+	private static final By productCatageroyLocator = By.xpath("//div[@id='block_top_menu']/ul/li/a");
+	private static final By logoImageLocator = By.xpath("//img[@class='logo img-responsive']");
+
+
+	public LandingPagePageObject(WebDriver driver,Scenario scn) 
 	{
-	this.driver = driver;
+		this.driver = driver;
+		this.scn = scn;
 	}
-	private By productCatageroyLocator = By.xpath("//div[@id='block_top_menu']/ul/li/a");
 
 
 	public void productlist() {
 
-	List <WebElement> productList = driver.findElements(productCatageroyLocator);
-	
-	for(WebElement e : productList)
-	   {System.out.println(e);}
-	
+		List <WebElement> productList = driver.findElements(productCatageroyLocator);
+
+		System.out.println("Product list :: ");
+		//scn.log("Product list :: ");
+		for(WebElement e : productList)
+
+		{
+			System.out.println("          "+ e.getText());
+			// scn.log("          "+ e.getText());
+		}
+
 	}
 
-
+	public void LogoDisplay() 
+	{
+		boolean logoDisplayOrNOt =  driver.findElement(logoImageLocator).isDisplayed();
+		//   scn.log("Logo is Display =>"+ logoDisplayOrNOt);
+	}
 
 
 
